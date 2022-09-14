e70d35adade296d7fb2090f8f909dd57095876c0
Juan Camilo Salazar <juancspace@outlook.com>
2022-09-14 16:30:33 -0500

    Implementing blinding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
   name = 'Juan';
   age = 26;
   image = 'https://tresubresdobles.com/wp-content/uploads/2019/08/skft-912381dcd5b2c45c4a9ce8acf32cfd8c-768x961.jpg';
+  btnDisabled = true;
+
+  person = {
+    name: 'Juano',
+    age: 26,
+    avatar: 'https://tresubresdobles.com/wp-content/uploads/2019/08/skft-912381dcd5b2c45c4a9ce8acf32cfd8c-768x961.jpg'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
 }
