52897ba007c2e50e889675291564255c2809694e
Juan Camilo Salazar <juancspace@outlook.com>
2022-09-14 17:27:53 -0500

    Control structures in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,9 @@ export class AppComponent {
     avatar: 'https://tresubresdobles.com/wp-content/uploads/2019/08/skft-912381dcd5b2c45c4a9ce8acf32cfd8c-768x961.jpg'
   }
 
+  names: string[] = ['Juan', 'Carlos', 'Santiago'];
+  newName: string = '';
+
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
@@ -33,5 +36,13 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
