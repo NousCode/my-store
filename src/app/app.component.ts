afdb5df0a7162cef9d5b1e3ab7ae09d5458543f1
Juan Camilo Salazar <juancspace@outlook.com>
2022-09-16 16:21:30 -0500

    Creating products component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,32 +6,7 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: '../assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: '../assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: '../assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: '../assets/images/books.jpg'
-    },
-  ];;
+
   imgParent = '';
 
   showImg = true;
