58f577fe2d83cb8df45908d6f2085bb1f441c13a
Juan Camilo Salazar <juancspace@outlook.com>
2022-09-25 10:44:56 -0500

    Practice authentication in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   imgParent = '';
-
   showImg = true;
+  token = '';
+
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) { }
 
   onLoaded(img: string) {
     console.log('Log padre', img);
@@ -17,5 +24,30 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.userService.create({
+      name: 'Carlos',
+      email: 'carlos@mail.com',
+      password: '12345'
+     })
+     .subscribe(rta => {
+      console.log(rta);
+     })
+  }
+
+  login() {
+    this.authService.login('carlos@mail.com', '12345')
+     .subscribe(rta => {
+      this.token = rta.access_token;
+     })
+  }
+
+  getProfile() {
+    this.authService.profile(this.token)
+    .subscribe(profile => {
+      console.log(profile);
+    });
   }
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StoreService } from '../../services/store.service';
-
+import { AuthService } from '../../services/auth.service';
+import { User } from 'src/app/models/user.model';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,9 +12,12 @@ export class NavComponent implements OnInit {
 
   activeMenu = false;
   counter = 0;
+  token = '';
+  profile: User | null = null;
 
   constructor(
-    private storeService : StoreService
+    private storeService : StoreService,
+    private authService : AuthService,
   ) { }
 
   ngOnInit(): void {
@@ -23,5 +28,34 @@ export class NavComponent implements OnInit {
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
+  }
+
+  login() {
+    this.authService.login('carlos@mail.com', '12345')
+    .subscribe(rta => {
+      console.log(rta.access_token);
+      this.token = rta.access_token;
+      this.getProfile();
+    })
+  }
+
+  getProfile() {
+    this.authService.profile(this.token)
+    .subscribe(user => {
+      this.profile = user;
+    });
+  }
+
+  loginProfile() {
+    this.authService.login('carlos@mail.com', '12345')
+    .pipe(
+      switchMap((actualToken) => {
+        this.token = actualToken.access_token;
+        return this.authService.profile(this.token);
+      })
+    )
+    .subscribe(data => {
+      this.profile = data;
+    })
   }
 }
