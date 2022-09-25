c6d11496660b91616888933cca24f9c283d7e076
Juan Camilo Salazar <juancspace@outlook.com>
2022-09-25 13:04:04 -0500

    Practice upload and download files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { UsersService } from './services/users.service';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,10 @@ export class AppComponent {
 
   imgParent = '';
   showImg = true;
+  imgRta = '';
 
   constructor(
-    private authService: AuthService,
+    private fileService: FilesService,
     private userService: UsersService,
   ) { }
 
@@ -34,5 +36,22 @@ export class AppComponent {
      .subscribe(rta => {
       console.log(rta);
      })
+  }
+
+  downloadPDF(){
+    this.fileService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe()
+  }
+
+  onUpload(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if (file) {
+
+      this.fileService.uploadFile(file)
+      .subscribe(rta => {
+        this.imgRta = rta.location;
+      })
+    }
   }
 }
