ec33853da28075abc7621ba6bd9d7eee9572f9f6
Juan Camilo Salazar <juancspace@outlook.com>
2022-09-28 14:41:25 -0500

    Deactive guard.

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OnExit } from './../../../guards/exit.guard';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.sass']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnExit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  onExit() {
+    const rta = confirm('Lógica desde el componente, estas seguro de salir?');
+    return rta;
   }
 
 }
